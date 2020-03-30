14bb0f860660d15c30b61e2d29897390e58f816a
Glitch (meething-webrtc-gun) <none>
2020-03-30 15:39:21 +0000

    🚧🌱 Checkpoint ./src/assets/js/rtc.js:13690106/13424

--- src/assets/js/rtc.js
+++ src/assets/js/rtc.js
@@ -34,14 +34,8 @@ window.addEventListener('load', ()=>{
 	var peers = ['https://gunmeetingserver.herokuapp.com/gun'];
 	var opt = { peers: peers, localStorage: false, radisk: false };
 	var socket = Gun(opt).get('rtcmeeting').get(room);
-  window.GUN = socket;
-
-        var socketId = h.uuidv4();
-        var myStream = '';
-      
-      console.log('Starting! you are',socketId);
-
-	socket.emit = function(key,value){
+  // Custom Emit Function
+  socket.emit = function(key,value){
 		if((value.sender && value.to)&&value.sender==value.to) return;
 		console.log('debug emit key',key,'value',value);
 		if(!key||!value) return;
@@ -49,101 +43,106 @@ window.addEventListener('load', ()=>{
 		if(key=="sdp"||key=="icecandidates") value = JSON.stringify(value);
 		socket.get(key).put(value);
 	}
+  window.GUN = socket;
 
-	// Initialize Session
+      var socketId = h.uuidv4();
+      var myStream = '';
+      
+      console.log('Starting! you are',socketId);
 
-  socket.emit('subscribe', {
+	    // Initialize Session
+      socket.emit('subscribe', {
                 room: room,
                 socketId: socketId,
 		            name: username || socketId
-  });
+      });
 
-	socket.get('subscribe').on(function(data,key){
-		if(data.ts && (Date.now() - data.ts) > TIMEGAP) return;
-    STATE.users[data.socketId] = data;
-    if(pc[data.socketId] !== undefined) {
-      return;
-    }
-	  if(data.socketId == socketId || data.sender == socketId) return;
-		console.log('got subscribe!',data);
-		socket.emit('newuser', {socketId:data.socketId});
-	});
+      socket.get('subscribe').on(function(data,key){
+        if(data.ts && (Date.now() - data.ts) > TIMEGAP) return;
+        STATE.users[data.socketId] = data;
+        if(pc[data.socketId] !== undefined) {
+          return;
+        }
+        if(data.socketId == socketId || data.sender == socketId) return;
+        console.log('got subscribe!',data);
+        socket.emit('newuser', {socketId:data.socketId});
+      });
 
-	socket.get('newuser').on(function(data,key){
-		if(data.ts && (Date.now() - data.ts) > TIMEGAP) return;
-	        if(data.socketId == socketId || data.sender == socketId) return;
-                socket.emit('newUserStart', {to:data.socketId, sender:socketId, name:data.name||data.socketId});
-                pc.push(data.socketId);
-                init(true, data.socketId);
-	});
+      socket.get('newuser').on(function(data,key){
+        if(data.ts && (Date.now() - data.ts) > TIMEGAP) return;
+              if(data.socketId == socketId || data.sender == socketId) return;
+                    socket.emit('newUserStart', {to:data.socketId, sender:socketId, name:data.name||data.socketId});
+                    pc.push(data.socketId);
+                    init(true, data.socketId);
+      });
 
-	socket.get('newUserStart').on(function(data,key){
-		if(data.ts && (Date.now() - data.ts) > TIMEGAP) return;
-	        if(data.socketId == socketId || data.sender == socketId) return;
-                pc.push(data.sender);
-                init(false, data.sender);
-	});
+      socket.get('newUserStart').on(function(data,key){
+        if(data.ts && (Date.now() - data.ts) > TIMEGAP) return;
+              if(data.socketId == socketId || data.sender == socketId) return;
+                    pc.push(data.sender);
+                    init(false, data.sender);
+      });
 
-	socket.get('icecandidates').on(function(data,key){
-		try {
-			data = JSON.parse(data);
-      console.log(data.sender.trim() + " is trying to connect with " + data.to.trim())      
-			if(data.ts && (Date.now() - data.ts) > TIMEGAP) return;
-			data.candidate = new RTCIceCandidate(data.candidate);
-			if (!data.candidate) return;
-		} catch(e){ console.log(e); return; };
-	        if(data.socketId == socketId || data.to != socketId) return;
-		      console.log('ice candidate',data);
-          data.candidate ? pc[data.sender].addIceCandidate(new RTCIceCandidate(data.candidate)) : '';
-	});
+      socket.get('icecandidates').on(function(data,key){
+        try {
+          data = JSON.parse(data);
+          console.log(data.sender.trim() + " is trying to connect with " + data.to.trim())      
+          if(data.ts && (Date.now() - data.ts) > TIMEGAP) return;
+          data.candidate = new RTCIceCandidate(data.candidate);
+          if (!data.candidate) return;
+        } catch(e){ console.log(e); return; };
+              if(data.socketId == socketId || data.to != socketId) return;
+              console.log('ice candidate',data);
+              data.candidate ? pc[data.sender].addIceCandidate(new RTCIceCandidate(data.candidate)) : '';
+      });
 
-	socket.get('sdp').on(function(data,key){
-		try {
-			data = JSON.parse(data);
-			if(data.ts && (Date.now() - data.ts) > TIMEGAP) return;
-		        if(!data || data.socketId == socketId || data.sender == socketId || !data.description ) return;
-			if(data.to !== socketId) {
-				console.log('not for us? dropping sdp');
-				return;
-			}
-		} catch(e) { console.log(e); return; }
+      socket.get('sdp').on(function(data,key){
+        try {
+          data = JSON.parse(data);
+          if(data.ts && (Date.now() - data.ts) > TIMEGAP) return;
+                if(!data || data.socketId == socketId || data.sender == socketId || !data.description ) return;
+          if(data.to !== socketId) {
+            console.log('not for us? dropping sdp');
+            return;
+          }
+        } catch(e) { console.log(e); return; }
 
-                if(data.description.type === 'offer'){
-                    data.description ? pc[data.sender].setRemoteDescription(new RTCSessionDescription(data.description)) : '';
+                    if(data.description.type === 'offer'){
+                        data.description ? pc[data.sender].setRemoteDescription(new RTCSessionDescription(data.description)) : '';
 
-                    h.getUserMedia().then(async (stream)=>{
-                        if(!document.getElementById('local').srcObject){
-                            document.getElementById('local').srcObject = stream;
-                        }
+                        h.getUserMedia().then(async (stream)=>{
+                            if(!document.getElementById('local').srcObject){
+                                document.getElementById('local').srcObject = stream;
+                            }
 
-                        //save my stream
-                        myStream = stream;
+                            //save my stream
+                            myStream = stream;
 
-                        stream.getTracks().forEach((track)=>{
-                            pc[data.sender].addTrack(track, stream);
+                            stream.getTracks().forEach((track)=>{
+                                pc[data.sender].addTrack(track, stream);
+                            });
+
+                            let answer = await pc[data.sender].createAnswer();
+                            pc[data.sender].setLocalDescription(answer);
+
+                            socket.emit('sdp', {description:pc[data.sender].localDescription, to:data.sender, sender:socketId});
+                        }).catch((e)=>{
+                            console.error(e);
                         });
+                    }
 
-                        let answer = await pc[data.sender].createAnswer();
-                        pc[data.sender].setLocalDescription(answer);
+                    else if(data.description.type === 'answer'){
+                        pc[data.sender].setRemoteDescription(new RTCSessionDescription(data.description));
+                    }
+      });
 
-                        socket.emit('sdp', {description:pc[data.sender].localDescription, to:data.sender, sender:socketId});
-                    }).catch((e)=>{
-                        console.error(e);
-                    });
-                }
-
-                else if(data.description.type === 'answer'){
-                    pc[data.sender].setRemoteDescription(new RTCSessionDescription(data.description));
-                }
-	});
-
-	socket.get('chat').on(function(data,key){
-	    if(data.ts && (Date.now() - data.ts) > TIMEGAP) return;
-      if(data.socketId == socketId || data.sender == socketId) return;
-	    if(data.sender == username) return;
-	    console.log('got chat',key,data);
-      h.addChat(data, 'remote');
-	})
+      socket.get('chat').on(function(data,key){
+          if(data.ts && (Date.now() - data.ts) > TIMEGAP) return;
+          if(data.socketId == socketId || data.sender == socketId) return;
+          if(data.sender == username) return;
+          console.log('got chat',key,data);
+          h.addChat(data, 'remote');
+      })
 
         function sendMsg(msg,local){
             let data = {
@@ -181,9 +180,7 @@ window.addEventListener('load', ()=>{
             if(createOffer){
                 pc[partnerName].onnegotiationneeded = async ()=>{
                     let offer = await pc[partnerName].createOffer();
-                    
                     await pc[partnerName].setLocalDescription(offer);
-
                     socket.emit('sdp', {description:pc[partnerName].localDescription, to:partnerName, sender:socketId});
                 };
             }
@@ -199,25 +196,23 @@ window.addEventListener('load', ()=>{
                 let str = e.streams[0];
                 if(document.getElementById(`${partnerName}-video`)){
                     document.getElementById(`${partnerName}-video`).srcObject = str;
-		    //When the video frame is clicked. This will enable picture-in-picture
-		    document.getElementById(`${partnerName}-video`).addEventListener('click', ()=>{
-		        if (!document.pictureInPictureElement) {
-		            document.getElementById(`${partnerName}-video`).requestPictureInPicture()
-		            .catch(error => {
-		                // Video failed to enter Picture-in-Picture mode.
-		                console.error(error);
-		            });
-		        }
-		        else {
-		            document.exitPictureInPicture()
-		            .catch(error => {
-		                // Video failed to leave Picture-in-Picture mode.
-		                console.error(error);
-		            });
-		        }
-		    });
-
-
+                    //When the video frame is clicked. This will enable picture-in-picture
+                    document.getElementById(`${partnerName}-video`).addEventListener('click', ()=>{
+                        if (!document.pictureInPictureElement) {
+                            document.getElementById(`${partnerName}-video`).requestPictureInPicture()
+                            .catch(error => {
+                                // Video failed to enter Picture-in-Picture mode.
+                                console.error(error);
+                            });
+                        }
+                        else {
+                            document.exitPictureInPicture()
+                            .catch(error => {
+                                // Video failed to leave Picture-in-Picture mode.
+                                console.error(error);
+                            });
+                        }
+                    });
                 }
 
                 else{
@@ -230,7 +225,7 @@ window.addEventListener('load', ()=>{
                     
                     // Video user title
                     var vtitle = document.createElement("p");
-                    var vuser = STATE.users[partnerName] ? STATE.users[partnerName].name : partnerName;
+                    var vuser = partnerName;
                     vtitle.innerHTML = `<center>${vuser}</center>`;
                     vtitle.id = `${partnerName}-title`
 
@@ -261,10 +256,10 @@ window.addEventListener('load', ()=>{
                 STATE.media[pc[partnerName]] = pc[partnerName].iceConnectionState;
                 switch(pc[partnerName].iceConnectionState){
                     case 'connected':
-                        sendMsg(partnerName+" is "+STATE.media[pc[partnerName]],local);
+                        sendMsg(partnerName+" is "+STATE.media[pc[partnerName]],true);
                         break;
                     case 'disconnected':
-                        sendMsg(partnerName+" is "+STATE.media[pc[partnerName]],local);
+                        sendMsg(partnerName+" is "+STATE.media[pc[partnerName]],true);
                         h.closeVideo(partnerName);
                         break;
                     case 'failed':
@@ -278,7 +273,6 @@ window.addEventListener('load', ()=>{
                       break;
                 }
             };
-
 
 
             pc[partnerName].onsignalingstatechange = (d)=>{
