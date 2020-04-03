368548afdb38e941cbc11ffb222b3a9c215968d2
Glitch (meething-webrtc-gun) <none>
2020-04-03 13:40:26 +0000

    👓🎙 Checkpoint ./src/assets/js/users.js:193094/706

--- src/assets/js/users.js
+++ src/assets/js/users.js
@@ -2,15 +2,13 @@ class Presence {
   constructor(gun, room) {
     this.gun = gun;
     this.users = gun.get("presence").get(room);
-    this.show();
+    this.users.map().on(this.show)
   }
 
-  show() {
-    this.users.map().on(function(user, id) {
+  show(user, id) {
       if (user !== null) {
         console.log(JSON.stringify(user));
       }
-    });
   }
 
   async getAllUsers() {
@@ -25,7 +23,7 @@ class Presence {
 
   addUser(user) {
     if (user instanceof User) {
-      this.users.get(user.id).put(user);
+      this.users.get(user.id).put({name : user.name, ud});
     } else {
       console.log("Not of type User");
     }
@@ -52,32 +50,27 @@ class User {
   }
 }
 
-
 var peers = ['https://livecodestream-us.herokuapp.com/gun','https://livecodestream-eu.herokuapp.com/gun'];
 var opt = { peers: peers, localStorage: false, radisk: false };
 var gunDB = Gun(opt);
 
 var pid = sessionStorage.getItem("pid");
 if (pid == null || pid == undefined) {
-  pid = Gun()._.opt.pid;
+  pid = gunDB._.opt.pid;
   sessionStorage.setItem("pid", pid);
 }
 
-const meUser = new User("me", pid);
+const meUser = new User(pid, pid);
 const presence = new Presence(gunDB, "test");
-
-// var everyone = presence.getAllUsers().then(function(result) {
-//   console.log(result);
-// });
-
-// var me = presence.getUser(meUser.id).then(function(result) {
-//   console.log(result);
-// });
 
 window.onunload = window.onbeforeunload = function() {
   console.log("leaving " + pid);
+  meUser.online = false;
+  presence.remove(meUser.id);
 };
 
 window.onload = function(e) {
   console.log("entering " + pid);
+  meUser.online = true;
+  presence.addUser(meUser);
 };
