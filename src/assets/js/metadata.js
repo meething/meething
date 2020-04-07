e3a001cb913bb2007c74e9374a75e978dd45e2b8
Glitch (meething-webrtc-gun) <none>
2020-04-07 16:04:34 +0000

    🚉⏰ Checkpoint ./src/assets/js/metadata.js:193094/27417 ./src/index.html:193094/64

--- src/assets/js/metadata.js
+++ src/assets/js/metadata.js
@@ -2,27 +2,9 @@ const DB_RECORD_META = 'gunmeeting_metadata'
 const CUE_SHOW_TIME = 4;
 let USER_METADATA = 'metadata_' + STREAM_ID;
 
-class MetaData {
-  constructor(gun) {
-    this.gunDB = gun;
-  }
-}
-
-function sentMessage() {
-    var input = document.getElementById("message");
-    sentControlData({ text: input.value, like: false });
-    input.value = "";
-}
-
-function sentControlData(data) {
-    data.streamId = STREAM_ID;
-    let metaData = gunDB.get(USER_METADATA).put(data);
-    gunDB.get(DB_RECORD_META).set(metaData);
-}
-
-//Listen on startup
-receiveData();
-function receiveData() {
+var metaData = (function() {
+  "use strict";
+  
 
     gunDB.get(DB_RECORD_META).map().on(function (data, id) {
         if (!data) {
@@ -35,36 +17,29 @@ function receiveData() {
         if (data.text) {
             SETCLUE(data.text, tab, 1);
         }
+
+        if (data.like && data.streamId == STREAM_ID) {
+            sentLove();
+        }
     });
 }
 
-function SETCLUE(cue, element, hide) {
-    if (element) {
-        var video = document.getElementById(element);
-    } else {
-        var video = document.getElementById("record_video");
-    }
-    if (hide) HIDETRACKS(element);
-    if (cue == 0) return;
-    if (!video) return;
-    var track = video.addTextTrack("captions", "English", "en");
-    if (!track) return;
-    var time = parseInt(video.currentTime);
-    track.mode = "showing";
-    // track.addCue(new VTTCue(0, 99999, cue|| "..." ));
-    track.addCue(new VTTCue(time, time + CUE_SHOW_TIME, cue || "..."));
-};
+  // Create the methods object
+  var methods = {};
 
-function HIDETRACKS(element) {
-    if (element) {
-        var video = document.getElementById(element);
-    } else {
-        var video = document.getElementById("record_video");
-    }
-    if (!video) return;
-    if (!video.textTracks) return;
-    // Oddly, there's no way to remove a track from a video, so hide them instead
-    for (i = 0; i < video.textTracks.length; i++) {
-        video.textTracks[i].mode = "hidden";
-    }
-};
+  //
+  // Methods
+  //
+  methods.sentMessage = function(message) {
+    this.sentControlData({ text: message });
+  };
+
+  methods.sentControlData = function(data) {
+    data.streamId = STREAM_ID;
+    let metaData = gunDB.get(USER_METADATA).put(data);
+    gunDB.get(DB_RECORD_META).set(metaData);
+  };
+
+  // Expose the public methods
+  return methods;
+})();
