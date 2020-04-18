7aa93a33d18b0613aaf6578aa1c62b3a464f3ae7
Glitch (meething-webrtc-gun) <none>
2020-04-18 12:55:34 +0000

    🐫👦 Checkpoint ./src/assets/js/helpers.js:13690106/781 ./src/assets/js/rtc.js:13690106/11

--- src/assets/js/helpers.js
+++ src/assets/js/helpers.js
@@ -85,10 +85,16 @@ export default {
   },
 
   getIceServer() {
+    try {
+     var pc = new RTCPeerConnection()
+       var iceServers = RTCPeerConnection.getDefaultIceServers();
+       if (iceServers) { console.log(iceServers); return iceServers; }
+    } catch(e){ console.log(e); }
+    
     var servers = {
       iceServers: [
         { urls: "stun:stun.l.google.com:19302" },
-        { urls: "stun:stun.sipgate.net:3478"} /*,
+        { urls: "stun:stun.sipgate.net:3478"}/*,
           {urls: "stun:stun.stunprotocol.org"},
           {urls: "stun:stun.sipgate.net:10000"},
           {urls: "stun:217.10.68.152:10000"},
@@ -97,8 +103,7 @@ export default {
     };
     
     var xirsys = [{ urls: ["stun:eu-turn4.xirsys.com"] },
-        {
-          username: "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",
+        { username: "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",
           credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
           urls: [
             "turn:eu-turn4.xirsys.com:80?transport=udp",
--- src/assets/js/rtc.js
+++ src/assets/js/rtc.js
@@ -469,7 +469,7 @@ function init(createOffer, partnerName) {
   //send ice candidate to partnerNames
   pc[partnerName].onicecandidate = ({ candidate }) => {
     if (!candidate) {
-        console.log('!!! NO ICE CANDIDATES');
+        console.log('!!! NO ICE CANDIDATES', candidate);
         return;
     }
     damSocket.out("icecandidates", {
