b5fe8d5e311f0f6c29938dc10b9c2ed1863e59a3
Glitch (meething-webrtc-gun) <none>
2020-04-18 18:35:38 +0000

    🌞👽 Checkpoint ./src/assets/js/helpers.js:13690106/174

--- src/assets/js/helpers.js
+++ src/assets/js/helpers.js
@@ -87,8 +87,8 @@ export default {
   getIceServer() {
     
     var servers = {
-      bundlePolicy: 'max-bundle',
       iceServers: [
+        { urls: "stun:stun.hepic.tel:19302"},
         { urls: "stun:stun.l.google.com:19302" },
         { urls: "stun:stun.sipgate.net:3478"}/*,
           {urls: "stun:stun.stunprotocol.org"},
