cbf729271990d86733e343637bce839b57f3ab08
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2020-04-23 12:13:46 +0000

    local changes

--- src/assets/js/helpers.js
+++ src/assets/js/helpers.js
@@ -150,18 +150,15 @@ export default {
     //return servers;
     return {
       iceServers: [
-        { urls: ["stun:eu-turn4.xirsys.com"] },
+        { urls: ["stun:turn.hepic.tel"] },
+        { urls: ["stun:stun.l.google.com:19302"] },
         {
           username:
-            "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",
-          credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
+            "meething",
+          credential: "b0756813573c0e7f95b2ef667c75ace3",
           urls: [
-            "turn:eu-turn4.xirsys.com:80?transport=udp",
-            "turn:eu-turn4.xirsys.com:3478?transport=udp",
-            "turn:eu-turn4.xirsys.com:80?transport=tcp",
-            "turn:eu-turn4.xirsys.com:3478?transport=tcp",
-            "turns:eu-turn4.xirsys.com:443?transport=tcp",
-            "turns:eu-turn4.xirsys.com:5349?transport=tcp",
+            "turn:turn.hepic.tel",
+            "turns:turn.hepic.tel?transport=tcp",
           ],
         }
       ],
