babbc958ad5d7910a23f082100a710f83dfe0e3b
Dletta <jcduschletta@gmail.com>
2020-04-24 19:57:58 -0700

    d

--- src/assets/js/graph.js
+++ src/assets/js/graph.js
@@ -1,28 +1,30 @@
 export default class Graph {
+  constructor() {
+    this.nodes = [];
+    this.edges = [];
 
-  let nodes = [];
-  let edges = [];
+    // initiate webworker here
+    this.worker = new Worker('workerGraph.js');
+  }
 
-  // initiate webworker here
-  var worker = new Worker('workerGraph.js');
   // create listeners for results
-  function updateGraph () {
+  updateGraph () {
     console.log('sending update to webworker')
-    worker.postMessage({
-      nodes: nodes,
-      links: edges
+    this.worker.postMessage({
+      nodes: this.nodes,
+      links: this.edges
     });
   }
 
 
-  worker.onmessage = function(event) {
+  this.worker.onmessage = function(event) {
     switch (event.data.type) {
       case "tick": return ticked(event.data);
       case "end": return ended(event.data);
     }
   }
 
-  function add (msg) {
+  add(msg) {
 
     // TODO: handle different events, add should only execute if event is presence or enter
 
@@ -32,10 +34,10 @@ export default class Graph {
     let data = JSON.parse(msg.data);
     data = data[0];
     //find each PID and see if it already there
-    let indexPid = nodes.findIndex(msg.pid);
+    let indexPid = this.nodes.findIndex(msg.pid);
     // if not found
     if(indexPid<0) {
-      nodes.push(msg.pid); //if it's me and I am not here push me in
+      this.nodes.push(msg.pid); //if it's me and I am not here push me in
       for(let peer of data) {
         console.log(peer)
         //if this isn't the the client building the graph
@@ -44,27 +46,27 @@ export default class Graph {
           let edge = {};
           edge.source = msg.pid;
           edge.target = peer[0];
-          edges.push(edge);
+          this.edges.push(edge);
         }
       }
     }
     updateGraph();
   }
 
-  function remove(msg) {
+  remove(msg) {
     //TODO: remove should only execute if event is 'leave'
 
     // remove graph nodes and edges from metadata
-    let indexPid = nodes.findIndex(msg.pid);
+    let indexPid = this.nodes.findIndex(msg.pid);
     // actually in nodes
     if(indexPid >= 0) {
       // remove from nodes
-      nodes.splice(indexPid, 1);
+      this.nodes.splice(indexPid, 1);
     }
     //iterate over edges objects and remove any edges that are either from or to this pid
-    edges.forEach((edge, index, edges) => {
+    this.edges.forEach((edge, index, edges) => {
       if(edge.source == msg.pid || edge.target == msg.pid) {
-        edges.splice(index, 1);
+        this.edges.splice(index, 1);
       }
     })
     updateGraph()
