f70d5ed3fc97fe6808ee462f0557591a17670691
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2020-04-26 13:45:40 +0200

    Fix pm2 config example

--- meething.config.js
+++ meething.config.js
@@ -1,26 +1,12 @@
 module.exports = {
   apps : [{
-    name: 'Meething",
+    name: 'Meething',
     script: 'src/app.js',
-    watch: './src'
-  }],
-
-  deploy : {
-    production : {
+    watch: './src',
+    env : {
       SSL : true,
       SSLKEY : 'src/assets/privkey.pem',
       SSLCERT  : 'src/assets/fullchain.pm',
-      DEBUG : false,
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload meething.config.js --env production',
-      'pre-setup': ''
-    },
-    development : {
-      SSL : false,
-      DEBUG : true,
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload meething.config.js --env development',
-      'pre-setup': ''
-    }
-  }
+      DEBUG : false
+  }]
 };
