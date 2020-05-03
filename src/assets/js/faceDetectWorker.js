7f9238e726a9030c75b82c0b746d2c8fe41dc822
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2020-05-04 00:41:14 +0200

    working version from glitch

--- src/assets/js/faceDetectWorker.js
+++ src/assets/js/faceDetectWorker.js
@@ -31,69 +31,37 @@ var loaded = false
 faceapi.nets.tinyFaceDetector.loadFromUri('/assets/models').then(
     async () => {
             this.console.log("Try to load")
-            var test_canvas = new OffscreenCanvas(90, 90)
-            const detections = await faceapi.detectSingleFace(test_canvas,
-                new faceapi.TinyFaceDetectorOptions())
             loaded = true
             console.log('Model Loaded')
         },
         (err) => {
             console.log("failed")
             this.console.log(err)
-
         }
 );
 
-
-
 this.onmessage = async function handler(e) {
 
-    if (!loaded) {
-
-
-        return;
-    }
-
-    console.log("in worker !")
+    if (!loaded) return;
 
     this.console.log(e.data.videoFrame)
     if (e.data.videoFrame !== 'undefined') {
-        this.console.log("Got the frame !")
+        //this.console.log("Got the frame !")
         var frame = e.data.videoFrame
         this.displaySize = {
             width: e.data.width,
             height: e.data.height
         }
-        console.log('frame: ', e.data.videoFrame)
-        console.log('width: ', this.displaySize.width)
-        console.log('height: ', this.displaySize.height)
         var canvas = new OffscreenCanvas(this.displaySize.width, this.displaySize.height)
         faceapi.matchDimensions(canvas, displaySize)
-        // canvas.width = this.displaySize.width;
-        // canvas.height = this.displaySize.height;
         canvas.getContext('bitmaprenderer').transferFromImageBitmap(frame);
         try {
             const detections = await faceapi.detectSingleFace(canvas,
                 new faceapi.TinyFaceDetectorOptions())
-
+            
+            if (!detections) return;
             this.console.log('detections: ', detections)
-            /*
-               const detections = await faceapi.detectAllFaces(frame,
-                 new faceapi.SsdMobilenetv1Options())
-                 */
-
-
-            console.log('xmin : ' + detections._box._x)
-            console.log('ymin : ' + detections._box._y)
-            console.log('width : ' + detections._box._width)
-            console.log('height : ' + detections._box._height)
-
             const resizedDetections = faceapi.resizeResults(detections, displaySize)
-
-            console.log('xmin : ' + resizedDetections.box.x)
-            console.log('ymin : ' + resizedDetections.box.y)
-            console.log('width : ' + resizedDetections.box.width)
-            console.log('height : ' + resizedDetections.box.height)
 
             //Send Bounding box to main thread
             var message = {
@@ -104,7 +72,6 @@ this.onmessage = async function handler(e) {
                     height: resizedDetections.box.height
                 }
             }
-
             this.postMessage(message)
         } catch (err) {
             this.console.log(err)
