a52f7a6778c9efcefe6ce74e7a310666718f2d58
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2020-05-04 00:43:43 +0200

    Update faceDetection.js

--- src/assets/js/faceDetection.js
+++ src/assets/js/faceDetection.js
@@ -6,17 +6,13 @@ var container = document.getElementById("localContainer");
 const canvas_box = document.createElement("canvas");
 canvas_box.style.position = "absolute";
 canvas_box.style.zIndex = "1001"; //make detection appear in front of local video
-
 document.body.append(canvas_box);
 
 var webWorker = new Worker("/assets/js/faceDetectWorker.js");
-//console.log("Load the model");
-//faceapi.nets.tinyFaceDetector.loadFromUri("/assets/models");
+
+var running = false;
 
 video.addEventListener("play", () => {
-  //const canvas = faceapi.createCanvasFromMedia(video);
-  //faceapi.matchDimensions(canvas, displaySize);
-  //document.body.append(canvas);
   canvas_box.width = video.offsetWidth;
   canvas_box.height = video.offsetHeight;
   //Position the canvas on the video
@@ -26,44 +22,34 @@ video.addEventListener("play", () => {
   var ctx = canvas_box.getContext("2d");
   ctx.strokeStyle = "#0000FF";
   ctx.lineWidth = 7;
-
-
+  
   //Detect and crop function
   async function detectAndCrop() {
+    if (running) return;
     try {
-      console.log("creating canvas, calling worker");
-      const frame = await createImageBitmap(video);
-
-      vWidth = video.offsetWidth;
-      vHeight = video.offsetHeight;
-      console.log("video_width = ", vWidth);
-      console.log("video_height = ", vHeight);
+      
+      //console.log("creating canvas, calling worker");     
+      running = true;
+      var vWidth = video.width;
+      var vHeight = video.height;
+      
+      const frame = await createImageBitmap(video, {resizeWidth: vWidth, resizeHeight: vHeight});
 
       var message = {
         videoFrame: frame,
         width: vWidth,
         height: vHeight
       };
-      console.log("message:", message);
       webWorker.postMessage(message, [frame]);
-
+      running = false;
+      
       webWorker.onmessage = e => {
+        
         if (e.data.boundingBox !== "undefined") {
           var detectX = e.data.boundingBox.xmin;
           var detectY = e.data.boundingBox.ymin;
           var detectWidth = e.data.boundingBox.width;
           var detectHeight = e.data.boundingBox.height;
-
-          console.log("detectX = ", detectX);
-          console.log("detectY = ", detectY);
-          console.log("detectWidth = ", detectWidth);
-          console.log("detectHeight = ", detectHeight);
-
-          //Draw Detections
-          // ctx.clearRect(0, 0, canvas_box.width, canvas_box.height);
-          // ctx.beginPath();
-          // ctx.rect(detectX, detectY, detectWidth, detectHeight);
-          // ctx.stroke();
 
           //Crop - TODO - cropping done on receiver side
           var croppedSquareLength =
@@ -72,7 +58,7 @@ video.addEventListener("play", () => {
           container.style.height = croppedSquareLength + 50 + "px";
           container.style.borderRadius = "90%";
           container.style.clipPath = "circle(50%)";
-
+          
           video.style.clipPath = "none";
           console.log("detectX = ", detectX);
           console.log("detectY = ", detectY);
@@ -85,11 +71,7 @@ video.addEventListener("play", () => {
       console.log(err);
     }
 
-    setTimeout(detectAndCrop, 1000);
-
-    //Draw detections
-    // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    // faceapi.draw.drawDetections(canvas, resizedDetections)
+    setTimeout(detectAndCrop, 2000);
   }
 
   detectAndCrop();
