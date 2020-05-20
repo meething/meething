37d59f0aa41dce699cc4a0a875cff468297030cc
QVDev <feedbackandroid@gmail.com>
2020-05-20 19:20:56 +0200

    Fix small typo

--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -51,7 +51,7 @@ export default class Modal {
       self.modalFilled(_modal);
     })
     window.ee.on("local-video-loaded", function () {
-      if (med.localVideo != undefined && med.localVideo.srcObject && med.localVideo.classList.contains("clipped")) {
+      if (med.localVideo !== undefined && med.localVideo.srcObject && med.localVideo.classList.contains("clipped")) {
         med.initComm();
       }
     });
