ee15802d44f885e099b773e5d2bd11d6009bb4ed
jacovinus <jacovinus@gmail.com>
2020-05-30 15:39:02 +0200

    limit calculation

--- src/assets/js/faceposition.js
+++ src/assets/js/faceposition.js
@@ -4,15 +4,23 @@
     var vcont = document.getElementById(cont);
     var vrect = Math.floor( v.getBoundingClientRect().width);
     var vcontrect = Math.floor(vcont.getBoundingClientRect().width);
+    // get the left and the right of container 
+    var vright =  Math.floor( v.getBoundingClientRect().right);
+    var vcontright =  Math.floor( vcont.getBoundingClientRect().right);
+    var vleft =  Math.floor( v.getBoundingClientRect().left);
+    var vcontleft =  Math.floor( v.getBoundingClientRect().left);
     v.style.left = - vrect/2 + vcontrect/2 + 'px';  
+    var diff = vcontrect / 2 - vrect /2;
     var m = movement;
-    if (m > 100){
-        // move left
+    if (m > 100 && m < 200 && vright >= vcontright ){
+        // move 
         v.style.left = (parseInt(v.style.left,10) - (m - 100)) + 'px';
         console.log("moving left: ",v.style.left)
-    } else {
+        console.log("m ", m)
+    } else if(m < 100 && m > 0 && vleft <= vcontleft){
         // move right
-        v.style.left = (parseInt(v.style.left,10) + (100 - m)) + 'px';
+        v.style.left = (parseInt(v.style.left,10) + (100 - m)) + 'px';  
         console.log("moving right: ",v.style.left)
+        console.log("m ", m)
     }
   }
