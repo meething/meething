1410cd8d23bfedf1f4fdc34cfd0941c978f7d13f
QVDev <feedbackandroid@gmail.com>
2020-06-08 12:23:24 +0200

    Avoid using glitch multigun in production

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,5 +1,5 @@
 const config = {
-  multigun: "https://gundb-multiserver.glitch.me/",
+  multigun: "https://us.meething.space:8787/",
   wssFailover: "wss://us.meething.space:2345",
   autoload: false,
   meethrix:false,
